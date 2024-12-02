cbac23c6058a2fd126b088ee2261b297fefda0ee
Kate Novak <kje@cochleoid.com>
2024-12-02 12:36:53 -0500

    More SDOT Exploration, Looking Into Specific Trees

--- prepare_trees_of_seattle_csv.py
+++ prepare_trees_of_seattle_csv.py
@@ -8,8 +8,21 @@ from wcvp import WCVP
 
 print('Loading datasets...')
 
-wcvp = WCVP('wcvp_taxon.csv', 'wcvp_distribution.csv')
 sdot = SDOT('SDOT_Trees_CDL_20241119.csv')
+wcvp = WCVP('wcvp_taxon.csv', 'wcvp_distribution.csv')
+
+pairs = [
+    ('Acer truncatum x A platanoides', 'Acer truncatum var. platanoides'),
+]
+
+scientificnames, scientificnames_clean = zip(*pairs)
+alternative_scientific_names = pd.DataFrame({
+    'scientificname': scientificnames,
+    'scientificname_clean': scientificnames_clean,
+})
+print(alternative_scientific_names)
+import sys
+sys.exit(0)
 
 df = pd.DataFrame()
 df['scientificname'] = sdot.dfsdot['SCIENTIFIC_NAME']
@@ -39,17 +52,19 @@ dftaxon = wcvp.get_dftaxon_for_locality('Washington')
 dftaxon['indigenous'] = dftaxon.apply(lambda x: x['locality'] == 'Washington' and x['establishmentmeans'] != 'introduced', axis=1)
 
 trees = pd.merge(
-    sdot.dfsdot[['OBJECTID', 'SCIENTIFIC_NAME', 'x', 'y']],
-    dftaxon[['scientificname', 'indigenous']],
+    sdot.dfsdot[['OBJECTID', 'SCIENTIFIC_NAME', 'GENUS', 'x', 'y']],
+    dftaxon[['scientificname', 'locality', 'indigenous']],
     left_on='SCIENTIFIC_NAME',
     right_on='scientificname',
     how='left',
 )
 trees['indigenous'].infer_objects(copy=False)
-trees['scientificname'] = trees['SCIENTIFIC_NAME']
-trees['objectid'] = trees['OBJECTID']
 trees['indigenous'] = trees.apply(lambda x: x['indigenous'] == True, axis=1)  # Fill NaN with False. There's got to be a way to do this.
-trees = trees[['objectid', 'scientificname', 'indigenous', 'x', 'y']]
+trees['local'] = trees['locality'] == 'Washington'
+trees['scientificname'] = trees['SCIENTIFIC_NAME']
+trees['genus'] = trees['GENUS']
+trees['objectid'] = trees['OBJECTID']
+trees = trees[['objectid', 'scientificname', 'genus', 'local', 'indigenous', 'x', 'y']]
 
 os.makedirs('data', exist_ok=True)
 
@@ -59,6 +74,7 @@ print(f'Saved {len(trees)} records to {trees_csv}')
 trees_csv_metadata_json = 'data/trees_of_seattle.csv.metadata.json'
 with open(trees_csv_metadata_json, 'w') as fout:
     fout.write(json.dumps({
-        'version': '0',
+        'version': '1',
+        'record_count': len(trees),
     }, indent='  ') + '\n')
 print(f'Metadata saved to {trees_csv_metadata_json}')
