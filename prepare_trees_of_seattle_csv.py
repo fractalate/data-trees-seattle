50a36f866859d21c5c683c429d7f16b5753ddb80
Kate Novak <kje@cochleoid.com>
2024-12-07 07:35:38 -0500

    More Filtering of Data

--- prepare_trees_of_seattle_csv.py
+++ prepare_trees_of_seattle_csv.py
@@ -11,13 +11,12 @@ print('Loading datasets...')
 sdot = SDOT('SDOT_Trees_CDL_20241119.csv')
 wcvp = WCVP('wcvp_taxon.csv', 'wcvp_distribution.csv')
 
-pairs = [
-    ('Acer truncatum x A platanoides', 'Acer truncatum var. platanoides'),
-]
+print(f'Loaded {len(sdot.dfsdot)} trees from SDOT trees dataset.')
 
 df = pd.DataFrame()
 df['scientificname'] = sdot.dfsdot['SCIENTIFIC_NAME']
 wcvp.assert_all_scientificnames_values_have_one_establishment_means_in_locality(df, 'Washington')
+del df
 
 '''
 # This one will fail!
@@ -26,6 +25,12 @@ wcvp.assert_all_scientificnames_values_have_one_establishmentmeans_in_locality(d
 '''
 
 print('Checks passed!')
+
+# We can do this because of the call to wcvp.assert_all_scientificnames_values_have_one_establishment_means_in_locality()
+dftaxon = wcvp.get_dftaxon_for_locality('Washington')
+dftaxon['indigenous'] = dftaxon.apply(lambda x: x['locality'] == 'Washington' and x['establishmentmeans'] != 'introduced', axis=1)
+dftaxon = dftaxon[['scientificname', 'locality', 'indigenous']]
+dftaxon = dftaxon.drop_duplicates()
 
 '''
 plants = [
@@ -39,12 +44,38 @@ for plant in plants:
     print(plant, '-', wcvp.lookup_establishment_type('Washington', plant))
 '''
 
-dftaxon = wcvp.get_dftaxon_for_locality('Washington')
-dftaxon['indigenous'] = dftaxon.apply(lambda x: x['locality'] == 'Washington' and x['establishmentmeans'] != 'introduced', axis=1)
+print('Cleaning up SDOT trees dataset...')
+
+dfsdot = sdot.dfsdot
+discarded_record_count = 0
+dfsdot_size_original = len(dfsdot)
+
+dfsdot_size_before = len(dfsdot)
+dfsdot = dfsdot[
+    (dfsdot['SCIENTIFIC_NAME'] != 'Planting Site')
+    & (dfsdot['SCIENTIFIC_NAME'] != 'stump')
+]
+discarded_nonliving_trees_record_count = dfsdot_size_before - len(dfsdot)
+discarded_record_count += discarded_nonliving_trees_record_count
+print(f'Discarded {discarded_nonliving_trees_record_count} ({discarded_nonliving_trees_record_count / dfsdot_size_original * 100:4.1f}%) non-living trees from the dataset.')
+del dfsdot_size_before
+
+dfsdot_size_before = len(dfsdot)
+dfsdot = dfsdot[
+    (dfsdot['SCIENTIFIC_NAME'] != 'Unknown')
+]
+discarded_unknown_trees_record_count = dfsdot_size_before - len(dfsdot)
+discarded_record_count += discarded_unknown_trees_record_count
+print(f'Discarded {discarded_unknown_trees_record_count} ({discarded_unknown_trees_record_count / dfsdot_size_original * 100:4.1f}%) unknown trees from the dataset.')
+del dfsdot_size_before
+
+print(f'Total: discarded {discarded_record_count} ({discarded_record_count / dfsdot_size_original * 100:4.1f}%) records.')
+
+print(f'Merging {len(dfsdot)} SDOT trees records with {len(dftaxon)} WCVP taxonomy records...')
 
 trees = pd.merge(
-    sdot.dfsdot[['OBJECTID', 'SCIENTIFIC_NAME', 'GENUS', 'x', 'y']],
-    dftaxon[['scientificname', 'locality', 'indigenous']],
+    dfsdot[['OBJECTID', 'SCIENTIFIC_NAME', 'GENUS', 'x', 'y']],
+    dftaxon,
     left_on='SCIENTIFIC_NAME',
     right_on='scientificname',
     how='left',
@@ -57,6 +88,8 @@ trees['genus'] = trees['GENUS']
 trees['objectid'] = trees['OBJECTID']
 trees = trees[['objectid', 'scientificname', 'genus', 'local', 'indigenous', 'x', 'y']]
 
+assert len(trees) == len(dfsdot)
+
 os.makedirs('data', exist_ok=True)
 
 trees_csv = 'data/trees_of_seattle.csv'
@@ -65,7 +98,10 @@ print(f'Saved {len(trees)} records to {trees_csv}')
 trees_csv_metadata_json = 'data/trees_of_seattle.csv.metadata.json'
 with open(trees_csv_metadata_json, 'w') as fout:
     fout.write(json.dumps({
-        'version': '1',
+        'version': '2',
         'record_count': len(trees),
+        'discarded_record_count': discarded_record_count,
+        'discarded_nonliving_trees_record_count': discarded_nonliving_trees_record_count,
+        'discarded_unknown_trees_record_count': discarded_unknown_trees_record_count,
     }, indent='  ') + '\n')
 print(f'Metadata saved to {trees_csv_metadata_json}')
